61a65728722277ba8bcfbdb974f85d308a148c11
Charles <charles.v.he@gmail.com>
2016-10-27 11:19:45 +0800

    添加类加载器

--- javadoc-swagger-maven-plugin/src/main/java/charles/swagger/mavenplugin/GenerateMojo.java
+++ javadoc-swagger-maven-plugin/src/main/java/charles/swagger/mavenplugin/GenerateMojo.java
@@ -3,28 +3,69 @@ package charles.swagger.mavenplugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.ProjectBuilder;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
 
 /**
  * Created by Charles on 2016/10/26.
  */
-@Mojo(name = "generate")
+@Mojo(name = "generate", requiresDependencyResolution = ResolutionScope.RUNTIME)
 public class GenerateMojo extends AbstractMojo {
     @Parameter
     private String basePath;
 
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    private MavenProject project;
+
+    @Component
+    private ProjectBuilder projectBuilder;
+
+    @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        MavenProject project = (MavenProject) this.getPluginContext().get("project");
-        System.out.println("basePath: " + basePath);
-    }
+        try {
+            List<String> elements = project.getRuntimeClasspathElements();
+            URL[] urls = new URL[elements.size()];
+            for (int i = 0; i < elements.size(); i++) {
+                urls[i] = new File(elements.get(i)).toURI().toURL();
+            }
+            URLClassLoader classLoader = new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
 
-    public String getBasePath() {
-        return basePath;
-    }
+            String pattern = ResourceUtils.FILE_URL_PREFIX
+                    + project.getBuild().getOutputDirectory()
+                    + "/"
+                    + ClassUtils.convertClassNameToResourcePath(basePath) + "/**/*.class";
+            PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver = new PathMatchingResourcePatternResolver();
+            Resource[] resources = pathMatchingResourcePatternResolver.getResources(pattern);
+            MetadataReaderFactory readerFactory = new CachingMetadataReaderFactory(pathMatchingResourcePatternResolver);
+            for (Resource resource : resources) {
+                if (resource.isReadable()) {
+                    MetadataReader reader = readerFactory.getMetadataReader(resource);
+                    String className = reader.getClassMetadata().getClassName();
+                    Class<?> clazz = classLoader.loadClass(className);
+                    System.out.println(clazz);
+                }
+            }
 
-    public void setBasePath(String basePath) {
-        this.basePath = basePath;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
